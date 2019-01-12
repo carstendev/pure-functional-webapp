3879bdc900b18ad12d477e5df4b590da613b99e5
Carsten Timpert <carsten.timpert@freenet.de>
2019-01-12 11:32:09 +0100

    Updated to newest flyway version.

--- src/main/scala/io/app/database/Database.scala
+++ src/main/scala/io/app/database/Database.scala
@@ -7,6 +7,8 @@ import io.app.config.DatabaseConfig
 import org.flywaydb.core.Flyway
 import doobie.hikari._
 import doobie.util.ExecutionContexts
+import org.flywaydb.core.internal.logging.slf4j.Slf4jLogCreator
+import org.flywaydb.core.api.logging.LogFactory
 
 
 object Database {
@@ -37,23 +39,17 @@ object Database {
     } yield xa
 
 
-  def migrate[F[_]](config: DatabaseConfig)(implicit F: Sync[F]): F[Unit] = {
+  def migrate[F[_]](config: DatabaseConfig)(implicit F: Sync[F]): F[Unit] = F.delay {
 
-    F.delay {
+    LogFactory.setLogCreator(new Slf4jLogCreator())
+    val cl = Class.forName(config.driver).getClassLoader
 
-      val cl = Class.forName(config.driver).getClassLoader
+    Flyway.configure(cl).dataSource(
+      config.url,
+      config.user,
+      config.password
+    ).load().migrate()
 
-      val flyway = new Flyway(cl)
-
-      flyway.setDataSource(
-        config.url,
-        config.user,
-        config.password
-      )
-
-      flyway.migrate()
-
-      ()
-    }
+    ()
   }
 }
